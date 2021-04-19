904d60c6aa7a28cf1f1a6a0ed17de7cc8e1f57e9
pmanuel123 <manuprash@gmail.com>
2021-04-19 22:19:02 +0530

    commit

---
+++ src/test/java/com/test/myNewApp/MyNewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.myNewApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
